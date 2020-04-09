d76de343d629e2775e446e51d7d71fe3399128d9
Joe Bailey <baileyrva@gmail.com>
2020-04-09 14:55:01 -0400

    Wrapper component created and exported

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom"; 
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
-import Header from "./components/Header"; 
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 import "./App.css";
 
 class App extends Component {
@@ -10,8 +11,14 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <NavBar /> 
+          <NavBar />
           <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
         </div>
       </Router>
     );
